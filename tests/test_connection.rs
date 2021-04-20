6d34dc27526235eaaa53a1c26dfaf0feb6aab64a
laura-ding <48548375+laura-ding@users.noreply.github.com>
2021-04-20 15:07:33 +0800

    Add lib and interface (#1)
    * add lib and interface

---
+++ tests/test_connection.rs
@@ -0,0 +1,16 @@
+/* Copyright (c) 2021 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
+ */
+
+extern crate nebula_rust;
+
+#[cfg(test)]
+mod test_session {
+    use nebula_rust::gClient::Session::Session;
+
+    #[test]
+    fn new_session() {
+    }
+}
